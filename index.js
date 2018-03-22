ebba3ee53e48f4c6f5cd9707d1d9407a6a864b63
Jeremy <jeremy.duran@gmail.com>
2018-03-22 19:29:51 +0000

    changed port service

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const path = require('path');
 const app = express();
 const opn = require('opn');
 
+var port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(3000,function() {
-        console.log("Running at Port 3000");
-        opn('http://localhost:3000');
+app.listen(port,function() {
+        console.log("Running at Port " + port);
+        console.log("http://localhost:" + port);
+        opn('http://localhost:' + port);
     }
 );
